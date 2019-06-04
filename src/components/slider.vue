25c2023b0fb8087e339b792bccd644b50c97b786
nick <tjtj54tj@163.com>
2019-06-04 20:28:50 +0800

    加入基本功能：     1、根据总日期设置     2、设定日期范围     3、拖动日期范围返回时间

--- src/components/slider.vue
+++ src/components/slider.vue
@@ -1,9 +1,14 @@
 <template>
     <div class="slider" :style="styles">
         <div ref="sliderBox" class="slider-box">
-            <div :style="sliderMaskStyles" class="slider-mask"></div>
-            <div :style="sliderStyles1" class="slider-bar slider-bar1"></div>
-            <div :style="sliderStyles2" class="slider-bar slider-bar2"></div>
+            <div @dragstart="handleRangeDragStart"
+                 @drag="handleRangeDrag"
+                 @dragover="handleRangeDragOver"
+                 @dragend="handleRangeDragEnd"
+                 draggable="true" :style="sliderMaskStyles" class="slider-range">
+                <div class="slider-bar slider-bar1"></div>
+                <div class="slider-bar slider-bar2"></div>
+            </div>
         </div>
     </div>
 </template>
@@ -31,6 +36,7 @@
     data() {
       return {
         box: {
+          offsetLeft: 0,
           width: 0,
           height: 0
         },
@@ -41,6 +47,17 @@
         myDateRange: {
           startDate: null,
           endDate: null
+        },
+        drag: {
+          offsetX: 0,
+          startOffsetX: 0,
+          draggingOffsetX: 0,
+          clientX: 0,
+        },
+        sliderMaskStyles: {
+          left: 0,
+          width: 0,
+          height: 0
         }
       }
     },
@@ -54,9 +71,51 @@
       getDomAttr(){
         this.box.width = this.$refs.sliderBox.clientWidth
         this.box.height = this.$refs.sliderBox.clientHeight
+        this.box.offsetLeft = this.$refs.sliderBox.offsetLeft
       },
       resize(){
         this.getDomAttr()
+      },
+      handleRangeDragStart(ev){
+        // ev.effectAllowed = 'move'
+        this.drag.startOffsetX = ev.layerX
+
+        console.log('开始拖动 is: ', ev)
+        // console.log('开始拖动: ', JSON.stringify(this.drag.startOffsetX))
+        // let y = ev.offsetY
+        // console.log('start.')
+        // console.log('x is: ', JSON.stringify(x))
+        // console.log('y is: ', JSON.stringify(y))
+        // console.log('ev is: ', ev)
+      },
+      handleRangeDrag(ev){
+        ev.preventDefault();
+        console.log('拖动: ', ev)
+        this.drag.draggingOffsetX = ev.layerX
+        this.drag.clientX = ev.clientX
+        // console.log('this.drag.draggingOffsetX is: ', JSON.stringify(this.drag.draggingOffsetX))
+        // console.log('this.drag.draggingOffsetX is: ', JSON.stringify(this.drag.draggingOffsetX))
+        console.log('this.drag.draggingOffsetX-this.drag.startOffsetX is: ', JSON.stringify(this.drag.draggingOffsetX))
+        // console.log('this.sliderMaskStyles is: ', JSON.stringify(this.sliderMaskStyles))
+        // let y = ev.offsetY
+        // console.log('x is: ', JSON.stringify(x))
+        // console.log('y is: ', JSON.stringify(y))
+        // console.log('ev is: ', ev)
+
+        let left = (this.drag.clientX-this.box.offsetLeft-this.drag.startOffsetX)
+        let left2 = left+parseFloat(this.sliderMaskStyles.width)
+        this.sliderMaskStyles.left = left+'px'
+        let startDateTime = (left*this.scale)+new Date(this.myTotalDateRange.startDate).getTime()
+        let endDateTime = (left2*this.scale)+new Date(this.myTotalDateRange.startDate).getTime()
+        console.log('startDateTime is: ', JSON.stringify(Math.ceil(startDateTime)))
+        console.log('endDateTime is: ', JSON.stringify(Math.ceil(endDateTime)))
+      },
+      handleRangeDragOver(ev) {
+      },
+      handleRangeDragEnd(){
+
+        // this.drag.startOffsetX = 0
+        // this.drag.draggingOffsetX = 0
       }
     },
     computed: {
@@ -102,14 +161,27 @@
         let endDate = this.myDateRange.endDate-this.myTotalDateRange.startDate
         return endDate/this.scale
       },
-      sliderMaskStyles(){
-        let styles = {}
-        if (this.box.height&&this.slider1X&&this.slider2X) {
-          styles.height = this.box.height+'px'
-          styles.left = this.slider1X+'px'
-          styles.width = (this.slider2X-this.slider1X)+'px'
-        }
-        return styles
+      // sliderMaskStyles(){
+      //   let styles = {}
+      //   if (this.box.height&&this.slider1X&&this.slider2X) {
+      //     styles.height = this.box.height+'px'
+      //     // styles.left = this.slider1X+'px'
+      //     // styles.left = (this.slider1X+this.drag.draggingOffsetX-this.drag.startOffsetX)+'px'
+      //     styles.left = (this.slider1X+this.drag.clientX-this.drag.draggingOffsetX-this.drag.startOffsetX)+'px'
+      //     styles.width = (this.slider2X-this.slider1X)+'px'
+      //   }
+      //   return styles
+      // }
+    },
+    watch: {
+      'box.height'(h) {
+        this.sliderMaskStyles.height = h+'px'
+      },
+      slider1X(x) {
+        this.sliderMaskStyles.left = x+'px'
+      },
+      slider2X(x) {
+        this.sliderMaskStyles.width = (x-this.slider1X)+'px'
       }
     },
     created(){
@@ -138,13 +210,15 @@
         position: relative;
         display: flex;
     }
-    .slider-box>.slider-bar{
-        position: absolute;
-        width: 5px;
-        background-color: red;
-    }
-    .slider-mask{
+    .slider-range{
         position: absolute;
         background-color: rgba(0, 0, 0, 0.2);
+        display: flex;
+        justify-content: space-between;
+        cursor: ew-resize;
+    }
+    .slider-range>.slider-bar{
+        width: 5px;
+        background-color: red;
     }
 </style>
